861abe8c9dd37b567ac584f55e278a52f2ee20c5
paullee73 <paulleechat3@gmail.com>
2018-09-01 19:47:56 -0400

    4.4

--- ch4/4.4.cpp
+++ ch4/4.4.cpp
@@ -7,26 +7,46 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-class Solution
-{
-  public:
-    int depth(TreeNode *root)
-    {
-        if (root == NULL)
-        {
+// class Solution {
+// public:
+//     int depth(TreeNode* root){
+//         if(root == NULL){
+//             return 0;
+//         }
+//         return max(depth(root->left)+1, depth(root->right)+1);
+//     }
+    
+//     bool isBalanced(TreeNode* root) {
+//         if(root == NULL){
+//             return true;
+//         }
+//         int left = depth(root->left);
+//         int right = depth(root->right);
+//         return isBalanced(root->left) && isBalanced(root->right) && abs(left - right) <= 1;
+//     }
+// };
+
+class Solution {
+public:
+    int helper(TreeNode* root){
+        if(root == NULL){
             return 0;
         }
-        return max(depth(root->left) + 1, depth(root->right) + 1);
-    }
-
-    bool isBalanced(TreeNode *root)
-    {
-        if (root == NULL)
-        {
-            return true;
+        int leftDepth = helper(root->left);
+        if(leftDepth == -1){
+            return -1;
         }
-        int left = depth(root->left);
-        int right = depth(root->right);
-        return isBalanced(root->left) && isBalanced(root->right) && abs(left - right) <= 1;
+        int rightDepth = helper(root->right);
+        if(rightDepth == -1){
+            return -1;
+        }
+        if(abs(leftDepth - rightDepth) > 1){
+            return -1;
+        }
+        return max(leftDepth, rightDepth)+1;
+    }
+    
+    bool isBalanced(TreeNode* root) {
+        return helper(root) != -1;
     }
 };
