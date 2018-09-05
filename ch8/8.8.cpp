590dfc24b973062b7a3e713b8b18c1c82219494a
paullee73 <paulleechat3@gmail.com>
2018-09-05 18:13:38 -0400

    8.8

--- ch8/8.8.cpp
+++ ch8/8.8.cpp
@@ -54,3 +54,34 @@ int main()
         cout << s << endl;
     }
 }
+
+
+
+
+class Solution {
+public:
+    void helper(vector<int> &nums, vector<vector<int>> &sol, vector<int> &tmp, unordered_map<int, bool> &used){
+        if(tmp.size() == nums.size()){
+            sol.push_back(tmp);
+        }
+        for(int i = 0; i < nums.size(); i++){
+            if(used[i] || i > 0 && nums[i] == nums[i-1] && !used[i-1]){
+                continue;
+            }
+            used[i] = true;
+            tmp.push_back(nums[i]);
+            helper(nums, sol, tmp, used);
+            used[i] = false;
+            tmp.pop_back();
+        }
+    }
+    
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> sol;
+        vector<int> tmp;
+        unordered_map<int, bool> used;
+        helper(nums, sol, tmp, used);
+        return sol;
+    }
+};
